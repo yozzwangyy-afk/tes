506ddeb6c1e398b52641bdd33d1e67394d6e19c8
yozzwangyy-afk <yozzwangyy@gmail.com>
2025-11-05 16:09:58 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
